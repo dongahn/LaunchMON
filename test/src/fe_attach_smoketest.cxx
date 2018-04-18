f8c38ec1dde142ed8f700c822c9a995cf736a08f
Dong H. Ahn <ahn1@llnl.gov>
2018-04-18 13:48:44 -0700

    cleanup: remove trailing whitespaces

--- test/src/fe_attach_smoketest.cxx
+++ test/src/fe_attach_smoketest.cxx
@@ -1,41 +1,41 @@
 /*
  * $Header: $
  *--------------------------------------------------------------------------------
- * Copyright (c) 2008, Lawrence Livermore National Security, LLC. Produced at 
- * the Lawrence Livermore National Laboratory. Written by Dong H. Ahn <ahn1@llnl.gov>. 
+ * Copyright (c) 2008, Lawrence Livermore National Security, LLC. Produced at
+ * the Lawrence Livermore National Laboratory. Written by Dong H. Ahn <ahn1@llnl.gov>.
  * LLNL-CODE-409469. All rights reserved.
  *
- * This file is part of LaunchMON. For details, see 
+ * This file is part of LaunchMON. For details, see
  * https://computing.llnl.gov/?set=resources&page=os_projects
  *
  * Please also read LICENSE.txt -- Our Notice and GNU Lesser General Public License.
  *
- * 
- * This program is free software; you can redistribute it and/or modify it under the 
- * terms of the GNU General Public License (as published by the Free Software 
+ *
+ * This program is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License (as published by the Free Software
  * Foundation) version 2.1 dated February 1999.
  *
- * This program is distributed in the hope that it will be useful, but WITHOUT ANY 
- * WARRANTY; without even the IMPLIED WARRANTY OF MERCHANTABILITY or 
- * FITNESS FOR A PARTICULAR PURPOSE. See the terms and conditions of the GNU 
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the IMPLIED WARRANTY OF MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the terms and conditions of the GNU
  * General Public License for more details.
  *
- * You should have received a copy of the GNU Lesser General Public License along 
- * with this program; if not, write to the Free Software Foundation, Inc., 59 Temple 
+ * You should have received a copy of the GNU Lesser General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc., 59 Temple
  * Place, Suite 330, Boston, MA 02111-1307 USA
- *--------------------------------------------------------------------------------			
+ *--------------------------------------------------------------------------------
  *
  *  Update Log:
  *        Jun  02 2009 DHA: Added LMON_STATUS_CB_TEST support.
  *        May  19 2009 DHA: Added LMON_ERROR_CB_TEST support.
- *        Mar  13 2009 DHA: Dynamic check for proctabsize 
- *        Jun  12 2008 DHA: Added GNU build system support.  
+ *        Mar  13 2009 DHA: Dynamic check for proctabsize
+ *        Jun  12 2008 DHA: Added GNU build system support.
  *        Mar  20 2008 DHA: Added BlueGene support.
  *        Mar  06 2008 DHA: Added calls-after-fail case.
- *        Mar  05 2008 DHA: Added Invalid pid test case to exercise 
+ *        Mar  05 2008 DHA: Added Invalid pid test case to exercise
  *                          connection timeout capability.
  *        Feb  09 2008 DHA: Added LLNS Copyright.
- *        Aug  06 2006 DHA: Adjust this case for minor API changes  
+ *        Aug  06 2006 DHA: Adjust this case for minor API changes
  *        Dec  27 2006 DHA: Created file.
  */
 
@@ -54,7 +54,7 @@
 #define ERROR_LOG_MAXSIZE 4096
 #define STRING_MAXSIZE 128
 
-#if MEASURE_TRACING_COST 
+#if MEASURE_TRACING_COST
 extern "C" {
   int begin_timer ();
   int time_stamp ( const char* description );
@@ -99,13 +99,13 @@ int errFunc (const char *format, va_list ap)
   char buf[ERROR_LOG_MAXSIZE];
 
   fprintf(stdout, "errFunc is called\n");
-  rc = vsnprintf(buf, ERROR_LOG_MAXSIZE, format, ap); 
+  rc = vsnprintf(buf, ERROR_LOG_MAXSIZE, format, ap);
   fprintf(stdout, "errFunc: %s\n", buf);
 
   return rc;
 }
 
-int 
+int
 main (int argc, char* argv[])
 {
   unsigned int i             = 0;
@@ -121,7 +121,7 @@ main (int argc, char* argv[])
 
   if ( argc < 3 )
     {
-      fprintf( stdout, 
+      fprintf( stdout,
 	"Usage: fe_launch_smoketest launcherpid daemonpath [daemonargs]\n");
       fprintf( stdout, "[LMON FE] FAILED\n");
       return EXIT_FAILURE;
@@ -129,7 +129,7 @@ main (int argc, char* argv[])
 
   if ( access(argv[2], X_OK) < 0 )
     {
-      fprintf ( stdout, 
+      fprintf ( stdout,
         "%s cannot be executed\n", argv[2] );
 
       fprintf ( stdout, "[LMON FE] FAILED\n" );
@@ -141,24 +141,24 @@ main (int argc, char* argv[])
       daemon_opts = argv+3;
     }
 
-  if ( ( rc = LMON_fe_init (LMON_VERSION)) 
+  if ( ( rc = LMON_fe_init (LMON_VERSION))
               != LMON_OK )
     {
       fprintf ( stdout, "[LMON FE] FAILED\n");
       return EXIT_FAILURE;
     }
-  
+
   if ( getenv ("LMON_ERROR_CB_TEST"))
     {
        if ( LMON_fe_regErrorCB(errFunc) != LMON_OK )
          {
             fprintf ( stdout, "[LMON FE] FAILED\n");
             return EXIT_FAILURE;
-         } 
-    } 
+         }
+    }
 
 
-  if ( ( rc = LMON_fe_createSession (&aSession)) 
+  if ( ( rc = LMON_fe_createSession (&aSession))
               != LMON_OK)
     {
       fprintf ( stdout, "[LMON FE] FAILED\n");
@@ -209,14 +209,14 @@ main (int argc, char* argv[])
                 {
                   fprintf ( stdout,
                   "[LMON FE] PASS: returned the LMON_ETOUT error code\n");
-                                                                                                                                                
+
                   if ( getenv ("LMON_CALLS_AFTER_FAIL_TEST") != NULL )
                     {
                       if ( (rc = LMON_fe_detach (aSession)) != LMON_OK )
                         {
                           fprintf (stdout,
                             "[LMON FE] PASS: LMON_fe_detach return with an error code [%d] \n", rc);
-                                                                                                                                                
+
                           return EXIT_SUCCESS;
                         }
                       else
@@ -224,20 +224,20 @@ main (int argc, char* argv[])
                           return EXIT_FAILURE;
                         }
                     } // if LMON_CALLS_AFTER_FAIL_TEST
-                                                                                                                                                
+
                    return EXIT_SUCCESS;
                 } // LMON_ETOUT returned
-            } // if LMON_INVALIDPID_TEST 
+            } // if LMON_INVALIDPID_TEST
 
             fprintf ( stdout,
               "[LMON FE] FAILED\n" );
             return EXIT_FAILURE;
         } // attachAndSpawn for remote
-    } 
+    }
   else
     {
-      if ( ( rc = LMON_fe_attachAndSpawnDaemons( 
-                aSession, 
+      if ( ( rc = LMON_fe_attachAndSpawnDaemons(
+                aSession,
 	        NULL,
 		spid,
 		argv[2],
@@ -259,7 +259,7 @@ main (int argc, char* argv[])
                         {
                           fprintf (stdout,
                             "[LMON FE] PASS: LMON_fe_detach return with an error code [%d] \n", rc);
-                                                                                                                                                
+
                           return EXIT_SUCCESS;
                         }
                       else
@@ -267,39 +267,39 @@ main (int argc, char* argv[])
                           return EXIT_FAILURE;
                         }
                     } // if LMON_CALLS_AFTER_FAIL_TEST
-                  
+
                    return EXIT_SUCCESS;
                 } // LMON_ETOUT returned
-            } // if LMON_INVALIDPID_TEST                                                                                                            
+            } // if LMON_INVALIDPID_TEST
             fprintf ( stdout, "[LMON FE] FAILED\n" );
             return EXIT_FAILURE;
         } // attachAndSpawn for local
-    } 
+    }
 #if MEASURE_TRACING_COST
   time_stamp ( "LMON_fe_attachAndSpawnDaemons perf" );
 #endif
 
-  if ( ( rc = LMON_fe_getProctableSize ( 
-                aSession, 
+  if ( ( rc = LMON_fe_getProctableSize (
+                aSession,
                 &proctabsize ))
               !=  LMON_OK )
     {
-       fprintf ( stdout, 
+       fprintf ( stdout,
          "[LMON FE] FAILED in LMON_fe_getProctableSize\n");
        return EXIT_FAILURE;
     }
 
-  if (proctabsize <= 0) 
+  if (proctabsize <= 0)
     {
-      fprintf ( stdout, 
-        "[LMON FE] FAILED, proctabsize is not equal to the given: %ud\n", 
+      fprintf ( stdout,
+        "[LMON FE] FAILED, proctabsize is not equal to the given: %ud\n",
         proctabsize);
       return EXIT_FAILURE;
     }
 
   proctab = (MPIR_PROCDESC_EXT*) malloc (
                 proctabsize*sizeof (MPIR_PROCDESC_EXT) );
-  
+
   if ( !proctab )
     {
        fprintf ( stdout, "[LMON FE] malloc returned null\n");
@@ -311,13 +311,13 @@ main (int argc, char* argv[])
 
 #if MEASURE_TRACING_COST
   begin_timer ();
-#endif 
+#endif
 
   if ( ( rc = LMON_fe_getProctable (
-                aSession, 
+                aSession,
                 proctab,
                 &psize,
-                proctabsize )) 
+                proctabsize ))
               !=  LMON_OK)
     {
        fprintf ( stdout, "[LMON FE] FAILED\n" );
@@ -377,7 +377,7 @@ main (int argc, char* argv[])
       fprintf ( stdout,
          "\n[LMON FE] RM launcher's pid is %d\n", rminfo.rm_launcher_pid);
    }
-  
+
   if ( (getenv ("LMON_FE_SHUTDOWNBE_TEST")) != NULL )
     {
       if ( ( rc = LMON_fe_shutdownDaemons ( aSession ) ) != LMON_OK )
@@ -427,7 +427,7 @@ main (int argc, char* argv[])
     }
 
   rc = LMON_fe_recvUsrDataBe ( aSession, NULL );
-                                                                                                                                               
+
   if ( (rc == LMON_EBDARG )
        || ( rc == LMON_ENOMEM )
        || ( rc == LMON_EINVAL ) )
@@ -447,9 +447,9 @@ main (int argc, char* argv[])
     }
 
   sleep (3); /* wait until all BE outputs are printed */
-                                                                                                                                               
+
   fprintf ( stdout,
     "\n[LMON FE] PASS: run through the end\n");
-                                                                                                                                               
+
   return EXIT_SUCCESS;
 }
